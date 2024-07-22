db768dc4e3d2f2f0b72b69793feee92062d7838f
Tomas Ramirez <40772172+tomasandre62@users.noreply.github.com>
2024-07-22 00:32:29 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,23 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+function generateRandomExcuse() {
+  const excuses = [
+    "Mi perro se comió mi tarea",
+    "Tuve una emergencia familiar",
+    "El perro del vecino se comió mi tarea mientras la llevaba a casa",
+    "Me enfermé",
+    "Olvidé que tenía tarea",
+    "No tuve tiempo para hacerla",
+    "Tuve problemas con el internet",
+    "No entendí la tarea",
+    "Tuve que ayudar a mi abuela a atrapar a un ladrón en su casa",
+    "Me quedé dormido estudiando y no me di cuenta de la hora"
+  ];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  const randomIndex = Math.floor(Math.random() * excuses.length);
+  return excuses[randomIndex];
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const excuse = generateRandomExcuse();
+  const heading = document.querySelector(".excusa"); // Selecciona el elemento h1
+  heading.textContent = excuse; // Asigna la excusa al contenido del h1
 };
